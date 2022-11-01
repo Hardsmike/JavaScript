0d3ebc278b7aff8999f0a3d2fb42f9a84d82c0d1
Hardsmike <arishnev.artem@gmail.com>
2022-11-01 13:58:44 +0200

    Add Homework №1

---
+++ Lesson 1/main.js
@@ -0,0 +1,39 @@
+let one = 'hello'
+let two = 'owu'
+let three = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let g = -999
+let h = 123
+let PI = 3.14
+let i = 2.7
+let j = 16
+let t = 5 > 4;
+let z = 5 < 4;
+
+console.log(one, two, three, d, e, f, g, h, PI, i, j, t, z);
+
+let firstName = 'Sviatoslav';
+let middleName = 'Vitaliyovich';
+let lastName = 'Kredensyr';
+let person = `${firstName}  ${middleName}  ${lastName}`
+
+console.log(person);
+
+
+let a = 100;
+let b = '100'
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+let name = prompt('Як тебе звати?')
+let midName = prompt('Яке твоє прізвище?')
+let ltName = prompt(`Як ти по батькові?`)
+let user = `${name} ${midName} ${ltName}`;
+
+console.log(user)
