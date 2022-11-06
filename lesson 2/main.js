af9da5fb95f9afbcfaeb5d9ddcbb94fd360b7849
Hardsmike <arishnev.artem@gmail.com>
2022-11-06 15:58:39 +0200

    Add HomeWork JS №2

---
+++ lesson 2/main.js
@@ -0,0 +1,158 @@
+// ------FIRST TASK-------
+const array = [100, 20, 'array', true, false, 102030, 'hey', 'no matter', 'ivan', 3];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
+
+
+//--------SECOND TASK--------
+let bookOne = {
+    title: 'One',
+    pageCount: 282,
+    genre: 'EN'
+}
+console.log(bookOne);
+
+let bookTwo = {
+    title: 'Two',
+    pageCount: 151,
+    genre: 'UA'
+}
+console.log(bookTwo);
+
+let bookThree = {
+    title: 'Three',
+    pageCount: 62,
+    genre: 'GB'
+}
+console.log(bookThree);
+
+
+
+//------THIRD TASK--------
+let book1 = {
+    title: 'One',
+    pageCount: 282,
+    genre: 'EN',
+    authors: [{
+        name: 'Max',
+        age: 27,
+    }]
+}
+console.log(book1);
+
+let book2 = {
+    title: 'Two',
+    pageCount: 151,
+    genre: 'UA',
+    authors: [{
+        name: 'Anthony',
+        age: 51,
+    }, 'Test']
+}
+console.log(book2.authors[1]);
+
+let book3 = {
+    title: 'Three',
+    pageCount: 62,
+    genre: 'GB',
+    authors: [{
+        name: 'Steven',
+        age: 43,
+    }]
+}
+console.log(book3);
+
+
+
+//------FOURTH TASK--------
+const arr = [
+    user1 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password1',
+    },
+
+    user2 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password2',
+    },
+
+    user3 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password3',
+    },
+
+    user4 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password4',
+    },
+
+    user5 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password5',
+    },
+
+    user6 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password6',
+    },
+
+    user7 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password7',
+    },
+
+    user8 = {
+        name: 'Ivan',
+        username: 'Quokka',
+        password: 'password8',
+    },
+
+    user9 = {
+        name: 'Olha',
+        username: 'Qaka',
+        password: 'password9',
+    },
+
+    user10 = {
+        name: 'Stepan',
+        username: 'Quo',
+        password: 'password10',
+    },
+];
+
+console.log(arr[0].password);
+console.log(arr[1].password);
+console.log(arr[2].password);
+console.log(arr[3].password);
+console.log(arr[4].password);
+console.log(arr[5].password);
+console.log(arr[6].password);
+console.log(arr[7].password);
+console.log(arr[8].password);
+console.log(arr[9].password);
+
+
+
+
+
+
+
+
+
+
+
